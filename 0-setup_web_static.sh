1180b0231aa24dc9bc7e61b67e8f6e8d4488ea2e
crysart <idukrystal@gmail.com>
2023-01-25 19:23:26 +0100

    attemp task 0 -5

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -17,7 +17,7 @@ sudo chown ubuntu:ubuntu  /data/ -R
 
 string="server_name _;\n\n\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t\ttry_files \$uri \$uri/ =404;\n\t}"
 
-sed -i "s?server_name _;?$string?" /etc/nginx/sites-enabled/default*
+sudo sed -i "s?server_name _;?$string?" /etc/nginx/sites-enabled/default*
 
 sudo service nginx restart
 
