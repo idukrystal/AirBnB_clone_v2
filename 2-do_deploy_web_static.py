4a7252a0fa60f97f253c24a2e6f933c3075cec3d
crysart <idukrystal@gmail.com>
2023-01-26 23:24:43 +0100

    task 3 01

---
+++ 2-do_deploy_web_static.py
@@ -0,0 +1,60 @@
+#!/usr/bin/python3
+"""
+Fabric script that distributes an archive to servers
+"""
+
+from datetime import datetime
+from fabric.api import *
+import os
+
+env.hosts = ["3.89.155.51", "100.26.169.4"]
+env.user = "ubuntu"
+
+
+def do_pack():
+    """Archives static files. return path if succesfull"""
+    if not os.path.isdir("versions"):
+        os.mkdir("versions")
+    d_time = datetime.now()
+    output = "versions/web_static_{}{}{}{}{}{}.tgz".format(
+        d_time.year,
+        d_time.month,
+        d_time.day,
+        d_time.hour,
+        d_time.minute,
+        d_time.second
+    )
+    try:
+        print("Packing web_static to {}".format(output))
+        local("tar -cvzf {} web_static".format(output))
+        size = os.stat(output).st_size
+        print("web_static packed: {} -> {} Bytes".format(output, size\
+))
+    except Exception:
+        output = None
+    return output
+
+
+def do_deploy(archive_path):
+    """
+        pushes archive to servers .
+    """
+    if os.path.exists(archive_path):
+        archived_file = archive_path[9:]
+        newest_version = "/data/web_static/releases/" + archived_file3[:-4]
+        archived_file = "/tmp/" + archived_file
+        put(archive_path, "/tmp/")
+        run("sudo mkdir -p {}".format(newest_version))
+        run("sudo tar -xzf {} -C {}/".format(archived_file,
+                                             newest_version))
+        run("sudo rm {}".format(archived_file))
+        run("sudo mv {}/web_static/* {}".format(newest_version,
+                                                newest_version))
+        run("sudo rm -rf {}/web_static".format(newest_version))
+        run("sudo rm -rf /data/web_static/current")
+        run("sudo ln -s {} /data/web_static/current".format(newest_version))
+
+        print("New version deployed!")
+        return True
+
+    return False
