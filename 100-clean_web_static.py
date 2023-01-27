644b446acecc44566e423779941de466cacaa2e4
crysart <idukrystal@gmail.com>
2023-01-27 01:43:11 +0100

    adv x1

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -5,7 +5,7 @@ from datetime import datetime
 from fabric.api import env, local, put, run, runs_once
 
 
-env.hosts = ['34.239.147.107', '52.3.247.207']
+env.hosts = ['3.89.155.51', '100.26.169.4']
 
 
 @runs_once
