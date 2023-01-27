12516a3e64a8c355c4662b3da88976ae6a51cab4
crysart <idukrystal@gmail.com>
2023-01-27 02:46:37 +0100

    yes o

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -3,8 +3,7 @@
 import os
 from fabric.api import *
 
-env.hosts = ["104.196.168.90", "35.196.46.172"]
-
+env.hosts = ["3.89.155.51", "100.26.169.4"]
 
 def do_clean(number=0):
     """Delete out-of-date archives.
